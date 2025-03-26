6de23b7fb9c133f811575962ade70d073db2f0b8
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2025-03-25 22:02:39 -0700

    Finish day 2 of Florbles: deserialization

--- devlog/week1.md
+++ devlog/week1.md
@@ -17,3 +17,6 @@ Defend the pentagon against the wacky dancing aliens!
 ## Log
 ### Day 1
 Found a fantastic resource on [icospheres](https://www.songho.ca/opengl/gl_sphere.html). I modified the code to simply generate an icosphere with 8 subdivisions and serialized the data to binary. Now, it should be possible to quickly deserialize at runtime. Once I have the data imported, I need to figure out how to render the sphere with SDL3's new graphics API. This is the biggest hurdle. Afterwards, I can customize the UV texture and start working on the actual game which shouldn't be too bad.
+
+### Day 2
+Did some research on gpu programming. The big challenge is pushing 3D operations onto the GPU with the SDL3 GPU API. I now have the model deserialized and stored into a struct. Now, how to draw the thing with UV mapped textures? We have arrays for the indices, vertices, normals, and uvs. We have a texture. The challenge is performing the proper transformations and drawing with the GPU. This might take up the rest of the week!
--- src/week1/game.c
+++ src/week1/game.c
@@ -8,23 +8,30 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#include <stdint.h>
+
+
+#define PATH_MAX 4096
 
 #define WINDOW_WIDTH  800
 #define WINDOW_HEIGHT 800
-
-#define WORLD_VERTEX_SIZE 2187   // packed (x,y,z)
-#define WORLD_NORMAL_SIZE 2187   // packed (x,y,z)
-#define WORLD_UV_SIZE     1458   // packed (u,v)
-#define WORLD_INDEX_SIZE  3840
 
 #define STRIDE 32
 
 
 typedef struct {
-	float *indices;
-	float *vertices;
-	float *normals;
-	float *uvs;
+	uint32_t indices_size;
+	uint32_t vertices_size;
+	uint32_t normals_size;
+	uint32_t uvs_size;
+	uint32_t index_count;
+	uint32_t vertex_count;
+	uint32_t normal_count;
+	uint32_t uv_count;
+	uint32_t *indices;
+	float *vertices; // jump 3: [(x,y,z)(x,y,z)...]
+	float *normals;  // jump 3: [(x,y,z)(x,y,z)...]
+	float *uvs;      // jump 2: [(u,v)(u,v)(u,v)...]
 } World;
 
 typedef struct {
@@ -41,6 +48,8 @@ typedef struct {
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
+	char *path = (char *) SDL_malloc(PATH_MAX * sizeof(char));
+
 	AppState *ctx = (AppState *)SDL_calloc(1, sizeof(AppState));
 	if (!ctx) {
 		return SDL_APP_FAILURE;
@@ -61,7 +70,59 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
+	/* Initialize Sphere */
+	int err = SDL_snprintf(path, PATH_MAX, "%ssphere.bin", SDL_GetBasePath());
+	if (err < 0) {
+		SDL_Log("Failure to write path to buffer.\n");
+		return SDL_APP_FAILURE;
+	}
+
+	size_t data_size = 0;
+	int offset = 0;
+	char *data = (char *) SDL_LoadFile(path, &(data_size));
+	if (!data) {
+		SDL_Log("Failure to load file at %s.\n", path);
+		return SDL_APP_FAILURE;
+	}
+
+	size_t indices_size = *(uint32_t *)(data + offset);
+	offset += 4;
+	ctx->world.indices = (uint32_t *)SDL_malloc(indices_size);
+	SDL_memcpy((void *)ctx->world.indices, (void *)(data + offset), indices_size);
+	offset += indices_size;
+
+	size_t vertices_size = *(uint32_t *)(data + offset);
+	offset += 4;
+	ctx->world.vertices = (float *)SDL_malloc(vertices_size);
+	SDL_memcpy((void *)ctx->world.vertices, (void *)(data + offset), vertices_size);
+	offset += vertices_size;
+
+	size_t normals_size = *(uint32_t *)(data + offset);
+	offset += 4;
+	ctx->world.normals = (float *)SDL_malloc(normals_size);
+	SDL_memcpy((void *)ctx->world.normals, (void *)(data + offset), normals_size);
+	offset += normals_size;
+
+	size_t uvs_size = *(uint32_t *)(data + offset);
+	offset += 4;
+	ctx->world.uvs = (float *)SDL_malloc(uvs_size);
+	SDL_memcpy((void *)ctx->world.uvs, (void *)(data + offset), uvs_size);
+
+	ctx->world.indices_size = indices_size;
+	ctx->world.vertices_size = vertices_size;
+	ctx->world.normals_size = normals_size;
+	ctx->world.uvs_size = uvs_size;
+
+	ctx->world.index_count = indices_size / 4;    // 4 bytes
+	ctx->world.vertex_count = vertices_size / 12; // 4 bytes per 3 coordinates
+	ctx->world.normal_count = normals_size / 12;  // 4 bytes per 3 coordinates
+	ctx->world.uv_count = uvs_size / 8;           // 4 bytes per 2 coordinates
+
+	SDL_free(data);
+
 	SDL_SetRenderVSync(ctx->renderer, SDL_RENDERER_VSYNC_ADAPTIVE);
+
+	SDL_free(path);
 
 	return SDL_APP_CONTINUE;
 }
@@ -92,6 +153,10 @@ void SDL_AppQuit(void *appstate, SDL_AppResult result)
 		//}
 
 		//TTF_Quit();
+		SDL_free(ctx->world.indices);
+		//SDL_free(ctx->world.vertices);
+		//SDL_free(ctx->world.normals);
+		///SDL_free(ctx->world.uvs);
 		SDL_free(ctx);
 	}
 }
